2a8fd8aab437949df186fae9e78d5d50321749af
Darryl Green <darryl.green@arm.com>
2019-07-10 11:59:10 +0100

    Move common compiler paths to separate file

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,5 +1,12 @@
 import groovy.transform.Field
 
+@Field compiler_paths = [
+    'gcc' : 'gcc',
+    'gcc48' : '/usr/local/bin/gcc48',
+    'clang' : 'clang',
+    'cc' : 'cc'
+]
+
 @Field one_platform = ["debian-9-x64"]
 @Field linux_platforms = ["debian-9-i386", "debian-9-x64"]
 @Field bsd_platforms = ["freebsd"]
