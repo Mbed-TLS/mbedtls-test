992ee9e6bdc629312f8dc6dfa64b96b2834785ed
Darryl Green <darryl.green@arm.com>
2019-07-10 11:59:02 +0100

    Move PR job scripts into separate file
    Scripts that are used for the PR jobs are currently duplicated
    between the jobs, with very minor differences between the scripts.
    Adjust the scripts so that they can be used by both jobs and move
    them into a separate file.

--- vars/mbedtls.groovy
+++ vars/mbedtls.groovy
@@ -1,107 +1,5 @@
 import groovy.transform.Field
 
-@Field std_make_test_sh = """\
-make clean
-CC=%s make
-make check
-./programs/test/selftest -x timing
-"""
-
-@Field gmake_test_sh = """\
-gmake clean
-CC=%s gmake
-gmake check
-./programs/test/selftest -x timing
-"""
-
-@Field cmake_test_sh = """\
-CC=%s  cmake -D CMAKE_BUILD_TYPE:String=Check .
-make clean
-make
-make test
-./programs/test/selftest -x timing
-"""
-
-@Field cmake_full_test_sh = """\
-CC=%s  cmake -D CMAKE_BUILD_TYPE:String=Check .
-make clean
-make
-make test
-./programs/test/selftest -x timing
-export PATH=/usr/local/openssl-1.0.2g/bin:/usr/local/gnutls-3.4.10/bin:\$PATH
-export SEED=1
-export LOG_FAILURE_ON_STDOUT=1
-./tests/compat.sh
-./tests/ssl-opt.sh
-./tests/scripts/test-ref-configs.pl
-"""
-
-@Field std_make_full_config_test_sh = """\
-make clean
-if [ ! -f "./tests/seedfile" ]; then
-    dd if=/dev/urandom of=./tests/seedfile bs=64 count=1
-fi
-if [ -d ./crypto -a ! -f "./crypto/tests/seedfile" ]; then
-    cp tests/seedfile crypto/tests/seedfile
-fi
-scripts/config.pl full
-scripts/config.pl unset MBEDTLS_MEMORY_BUFFER_ALLOC_C
-CC=%s make
-make check
-export PATH=/usr/local/openssl-1.0.2g/bin:/usr/local/gnutls-3.4.10/bin:\$PATH
-export SEED=1
-export LOG_FAILURE_ON_STDOUT=1
-./tests/ssl-opt.sh
-./tests/compat.sh
-"""
-
-@Field cmake_asan_test_sh = """\
-set +e
-if grep 'fno-sanitize-recover[^=]' CMakeLists.txt
-then
-    sed -i 's/fno-sanitize-recover/fno-sanitize-recover=undefined,integer/' CMakeLists.txt;
-fi
-set -e
-CC=%s cmake -D CMAKE_BUILD_TYPE:String=ASan .
-make
-make test
-./programs/test/selftest -x timing
-export PATH=/usr/local/openssl-1.0.2g/bin:/usr/local/gnutls-3.4.10/bin:\$PATH
-export SEED=1
-export LOG_FAILURE_ON_STDOUT=1
-./tests/compat.sh
-./tests/ssl-opt.sh
-./tests/scripts/test-ref-configs.pl
-"""
-
-@Field win32_mingw_test_bat = """\
-cmake . -G "MinGW Makefiles" -DCMAKE_C_COMPILER="gcc"
-mingw32-make
-mingw32-make test
-ctest -VV
-programs\\test\\selftest.exe
-"""
-
-@Field iar8_mingw_test_bat = """\
-perl scripts/config.pl baremetal
-cmake -D CMAKE_BUILD_TYPE:String=Check -DCMAKE_C_COMPILER="iccarm" -G "MinGW Makefiles" .
-mingw32-make lib
-"""
-
-@Field win32_msvc12_32_test_bat = """\
-call "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\vcvarsall.bat"
-cmake . -G "Visual Studio 12"
-MSBuild ALL_BUILD.vcxproj
-programs\\test\\Debug\\selftest.exe
-"""
-
-@Field win32_msvc12_64_test_bat = """\
-call "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\vcvarsall.bat"
-cmake . -G "Visual Studio 12 Win64"
-MSBuild ALL_BUILD.vcxproj
-programs\\test\\Debug\\selftest.exe
-"""
-
 @Field compiler_paths = [
     'gcc' : 'gcc',
     'gcc48' : '/usr/local/bin/gcc48',
@@ -301,39 +199,49 @@ def run_job() {
 
             /* Linux jobs */
             def jobs = gen_docker_jobs_foreach(
-                'std-make', linux_platforms, all_compilers, std_make_test_sh
+                'std-make', linux_platforms, all_compilers,
+                scripts.std_make_test_sh
             )
             jobs = jobs + gen_docker_jobs_foreach(
                 'std-make-full-config', linux_platforms, all_compilers,
-                std_make_full_config_test_sh
+                scripts.std_make_full_config_test_sh
             )
             jobs = jobs + gen_docker_jobs_foreach(
-                'cmake', linux_platforms, all_compilers, cmake_test_sh
+                'cmake', linux_platforms, all_compilers,
+                scripts.cmake_test_sh
             )
             jobs = jobs + gen_docker_jobs_foreach(
-                'cmake-full', linux_platforms, gcc_compilers, cmake_full_test_sh
+                'cmake-full', linux_platforms, gcc_compilers,
+                scripts.cmake_full_test_sh
             )
             jobs = jobs + gen_docker_jobs_foreach(
-                'cmake-asan', linux_platforms, asan_compilers, cmake_asan_test_sh
+                'cmake-asan', linux_platforms, asan_compilers,
+                scripts.cmake_asan_test_sh
             )
 
             /* BSD jobs */
             jobs = jobs + gen_node_jobs_foreach(
-                'gmake', bsd_platforms, bsd_compilers, gmake_test_sh
+                'gmake', bsd_platforms, bsd_compilers,
+                scripts.gmake_test_sh
             )
             jobs = jobs + gen_node_jobs_foreach(
-                'cmake', bsd_platforms, bsd_compilers, cmake_test_sh
+                'cmake', bsd_platforms, bsd_compilers,
+                scripts.cmake_test_sh
             )
 
             /* Windows jobs */
-            jobs = jobs + gen_windows_jobs('win32-mingw', win32_mingw_test_bat)
             jobs = jobs + gen_windows_jobs(
-                'win32_msvc12_32', win32_msvc12_32_test_bat
+                'win32-mingw', scripts.win32_mingw_test_bat
             )
             jobs = jobs + gen_windows_jobs(
-                'win32-msvc12_64', win32_msvc12_64_test_bat
+                'win32_msvc12_32', scripts.win32_msvc12_32_test_bat
             )
-            jobs = jobs + gen_windows_jobs('iar8-mingw', iar8_mingw_test_bat)
+            jobs = jobs + gen_windows_jobs(
+                'win32-msvc12_64', scripts.win32_msvc12_64_test_bat
+            )
+            jobs = jobs + gen_windows_jobs(
+                'iar8-mingw', scripts.iar8_mingw_test_bat
+            )
 
             /* All.sh jobs */
             dir('mbedtls') {
