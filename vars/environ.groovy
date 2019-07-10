1ff1f22498e7a3d21f8c09c44432a86209238ada
Darryl Green <darryl.green@arm.com>
2019-07-10 12:00:36 +0100

    Move example constants to common.groovy
    The parameters in the example map are changed from FOO to env.FOO.
    This is done as otherwise jobs that don't set these parameters will
    error. They will still be read in the same manner by the jobs that
    do set the parameters.

--- vars/environ.groovy
+++ vars/environ.groovy
@@ -15,6 +15,12 @@ def set_crypto_release_environment() {
     env.TARGET_REPO = 'crypto'
     env.REPO_TO_CHECKOUT = 'crypto'
     env.BRANCH_NAME = MBED_CRYPTO_BRANCH
+    if (TEST_MBED_OS_TLS_EXAMPLES == 'true') {
+        env.TEST_MBED_OS_AUTHCRYPT_EXAMPLE = 'true'
+        env.TEST_MBED_OS_BENCHMARK_EXAMPLE = 'true'
+        env.TEST_MBED_OS_HASHING_EXAMPLE = 'true'
+        env.TEST_MBED_OS_TLS_CLIENT_EXAMPLE = 'true'
+    }
 }
 
 def set_tls_release_environment() {
