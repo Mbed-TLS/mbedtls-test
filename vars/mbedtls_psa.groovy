34eac750368f1abbb6e7d535daaa7eb139b9c7a8
Darryl Green <darryl.green@arm.com>
2019-07-10 11:59:16 +0100

    Move Crypto testing stage into separate function

--- vars/mbedtls_psa.groovy
+++ vars/mbedtls_psa.groovy
@@ -225,6 +225,108 @@ def checkout_coverity_repo() {
                 credentialsId: "${env.GIT_CREDENTIALS_ID}"]]]
 }
 
+def run_crypto_tests() {
+    node {
+        try {
+            githubNotify context: 'Crypto Testing',
+                         description: 'In progress',
+                         status: 'PENDING'
+            deleteDir()
+
+            /* Linux jobs */
+            def jobs = gen_docker_jobs_foreach(
+                'std-make',
+                common.linux_platforms,
+                common.all_compilers,
+                scripts.std_make_test_sh
+            )
+            jobs = jobs + gen_docker_jobs_foreach(
+                'std-make-full-config',
+                common.linux_platforms,
+                common.all_compilers,
+                scripts.std_make_full_config_test_sh
+            )
+            jobs = jobs + gen_docker_jobs_foreach(
+                'cmake',
+                common.linux_platforms,
+                common.all_compilers,
+                scripts.cmake_test_sh
+            )
+            jobs = jobs + gen_docker_jobs_foreach(
+                'cmake-full',
+                common.linux_platforms,
+                common.gcc_compilers,
+                scripts.cmake_full_test_sh
+            )
+            jobs = jobs + gen_docker_jobs_foreach(
+                'cmake-asan',
+                common.linux_platforms,
+                common.asan_compilers,
+                scripts.cmake_asan_test_sh
+            )
+
+            /* BSD jobs */
+            jobs = jobs + gen_node_jobs_foreach(
+                'gmake',
+                common.bsd_platforms,
+                common.bsd_compilers,
+                scripts.gmake_test_sh
+            )
+            jobs = jobs + gen_node_jobs_foreach(
+                'cmake',
+                common.bsd_platforms,
+                common.bsd_compilers,
+                scripts.cmake_test_sh
+            )
+
+            /* Windows jobs */
+            jobs = jobs + gen_simple_windows_jobs(
+                'win32-mingw', scripts.win32_mingw_test_bat
+            )
+            jobs = jobs + gen_simple_windows_jobs(
+                'win32_msvc12_32', scripts.win32_msvc12_32_test_bat
+            )
+            jobs = jobs + gen_simple_windows_jobs(
+                'win32-msvc12_64', scripts.win32_msvc12_64_test_bat
+            )
+            jobs = jobs + gen_simple_windows_jobs(
+                'iar8-mingw', scripts.iar8_mingw_test_bat
+            )
+            for (build in ['mingw', '2013']) {
+                jobs = jobs + gen_windows_tests_jobs(build)
+            }
+
+            /* Coverity jobs */
+            jobs = jobs + gen_node_jobs_foreach(
+                'coverity',
+                common.coverity_platforms,
+                common.coverity_compilers,
+                scripts.std_coverity_sh
+            )
+
+            /* All.sh jobs */
+            for (component in all_sh_components) {
+                jobs = jobs + gen_all_sh_jobs('ubuntu-16.04', component)
+            }
+            jobs = jobs + gen_all_sh_jobs('ubuntu-18.04', 'build_mingw')
+
+            jobs = jobs + gen_abi_api_checking_job('ubuntu-16.04')
+
+            jobs.failFast = false
+            parallel jobs
+            githubNotify context: 'Crypto Testing',
+                         description: 'All tests passed',
+                         status: 'SUCCESS'
+        } catch (err) {
+            echo "Caught: ${err}"
+            currentBuild.result = 'FAILURE'
+            githubNotify context: 'Crypto Testing',
+                         description: 'Test failure',
+                         status: 'FAILURE'
+        }
+    }
+}
+
 /* main job */
 def run_job() {
     githubNotify context: 'Pre Test Checks',
@@ -276,105 +378,7 @@ def run_job() {
         }
     }
     stage('crypto-testing') {
-        node {
-            try {
-                githubNotify context: 'Crypto Testing',
-                             description: 'In progress',
-                             status: 'PENDING'
-                deleteDir()
-
-                /* Linux jobs */
-                def jobs = gen_docker_jobs_foreach(
-                    'std-make',
-                    common.linux_platforms,
-                    common.all_compilers,
-                    scripts.std_make_test_sh
-                )
-                jobs = jobs + gen_docker_jobs_foreach(
-                    'std-make-full-config',
-                    common.linux_platforms,
-                    common.all_compilers,
-                    scripts.std_make_full_config_test_sh
-                )
-                jobs = jobs + gen_docker_jobs_foreach(
-                    'cmake',
-                    common.linux_platforms,
-                    common.all_compilers,
-                    scripts.cmake_test_sh
-                )
-                jobs = jobs + gen_docker_jobs_foreach(
-                    'cmake-full',
-                    common.linux_platforms,
-                    common.gcc_compilers,
-                    scripts.cmake_full_test_sh
-                )
-                jobs = jobs + gen_docker_jobs_foreach(
-                    'cmake-asan',
-                    common.linux_platforms,
-                    common.asan_compilers,
-                    scripts.cmake_asan_test_sh
-                )
-
-                /* BSD jobs */
-                jobs = jobs + gen_node_jobs_foreach(
-                    'gmake',
-                    common.bsd_platforms,
-                    common.bsd_compilers,
-                    scripts.gmake_test_sh
-                )
-                jobs = jobs + gen_node_jobs_foreach(
-                    'cmake',
-                    common.bsd_platforms,
-                    common.bsd_compilers,
-                    scripts.cmake_test_sh
-                )
-
-                /* Windows jobs */
-                jobs = jobs + gen_simple_windows_jobs(
-                    'win32-mingw', scripts.win32_mingw_test_bat
-                )
-                jobs = jobs + gen_simple_windows_jobs(
-                    'win32_msvc12_32', scripts.win32_msvc12_32_test_bat
-                )
-                jobs = jobs + gen_simple_windows_jobs(
-                    'win32-msvc12_64', scripts.win32_msvc12_64_test_bat
-                )
-                jobs = jobs + gen_simple_windows_jobs(
-                    'iar8-mingw', scripts.iar8_mingw_test_bat
-                )
-                for (build in ['mingw', '2013']) {
-                    jobs = jobs + gen_windows_tests_jobs(build)
-                }
-
-                /* Coverity jobs */
-                jobs = jobs + gen_node_jobs_foreach(
-                    'coverity',
-                    common.coverity_platforms,
-                    common.coverity_compilers,
-                    scripts.std_coverity_sh
-                )
-
-                /* All.sh jobs */
-                for (component in all_sh_components) {
-                    jobs = jobs + gen_all_sh_jobs('ubuntu-16.04', component)
-                }
-                jobs = jobs + gen_all_sh_jobs('ubuntu-18.04', 'build_mingw')
-
-                jobs = jobs + gen_abi_api_checking_job('ubuntu-16.04')
-
-                jobs.failFast = false
-                parallel jobs
-                githubNotify context: 'Crypto Testing',
-                             description: 'All tests passed',
-                             status: 'SUCCESS'
-            } catch (err) {
-                echo "Caught: ${err}"
-                currentBuild.result = 'FAILURE'
-                githubNotify context: 'Crypto Testing',
-                             description: 'Test failure',
-                             status: 'FAILURE'
-            }
-        }
+        run_crypto_tests()
     }
     stage('tls-testing') {
         try {
