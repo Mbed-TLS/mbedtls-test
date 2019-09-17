acdb9e830ccb5e1b9825faa2e36692851aac330b
Gilles Peskine <Gilles.Peskine@arm.com>
2019-09-17 18:52:37 +0200

    Keep track of whether any outcome files are present
    Do nothing at the outcome file analysis stage if no outcome files were
    generated, which is the case on older branches without the outcome
    file feature.

--- vars/analysis.groovy
+++ vars/analysis.groovy
@@ -5,31 +5,37 @@ import groovy.transform.Field
 
 def stash_outcomes(job_name) {
     def stash_name = job_name + '-outcome'
-    stash(name: stash_name,
-          includes: '*-outcome.csv',
-          allowEmpty: true)
-    outcome_stashes.add(stash_name)
+    if (findFiles(glob: '*-outcome.csv')) {
+        stash(name: stash_name,
+              includes: '*-outcome.csv',
+              allowEmpty: true)
+        outcome_stashes.add(stash_name)
+    }
 }
 
 def gather_outcomes() {
     node("ubuntu-16.10-x64") {
-        dir('outcomes') {
-            deleteDir()
-            for (stash_name in outcome_stashes) {
-                unstash(stash_name)
+        // After running on an old branch which doesn't have the outcome
+        // file generation mechanism, or after running a partial run,
+        // there may not be any outcome file. In this case, silently
+        // do nothing.
+        if (!outcome_stashes.isEmpty()) {
+            dir('outcomes') {
+                deleteDir()
+                for (stash_name in outcome_stashes) {
+                    unstash(stash_name)
+                }
+                // Use separate commands, not a pipeline, to get an error
+                // if cat fails.
+                sh """
+    cat *.csv >../outcomes.csv
+    xz ../outcomes.csv
+    """
+                deleteDir()
             }
-            // If there are no outcome files, the cat invocation will fail.
-            // This should only happen when running a partial pipeline.
-            // Let it be an error so that it's apparent if something went
-            // wrong during a normal run.
-            sh """
-cat *.csv >../outcomes.csv
-xz ../outcomes.csv
-"""
-            deleteDir()
+            archiveArtifacts(artifacts: 'outcomes.csv.xz',
+                             fingerprint: true, allowEmptyArchive: true)
         }
-        archiveArtifacts(artifacts: 'outcomes.csv.xz',
-                         fingerprint: true, allowEmptyArchive: true)
     }
 }
 
