98e5b6566e003324a1c94ea331e1bfe2237843f8
Darryl Green <darryl.green@arm.com>
2019-10-03 11:29:18 +0100

    Deduplicate common PR environment setting code
    In preparation for enabling setting development and production
    environments for the PR job, we separate out common code into a
    separate function.

--- vars/environ.groovy
+++ vars/environ.groovy
@@ -2,39 +2,44 @@ def set_crypto_pr_environment() {
     env.JOB_TYPE = 'PR'
     env.TARGET_REPO = 'crypto'
     env.REPO_TO_CHECKOUT = 'crypto'
-    if (['development', 'feature-psa'].contains(CHANGE_TARGET)) {
-        env.TEST_MBED_OS_AUTHCRYPT_EXAMPLE = 'true'
-        env.TEST_MBED_OS_BENCHMARK_EXAMPLE = 'true'
-        env.TEST_MBED_OS_HASHING_EXAMPLE = 'true'
-        env.TEST_MBED_OS_TLS_CLIENT_EXAMPLE = 'true'
-        env.TEST_MBED_OS_CRYPTO_EXAMPLES = 'true'
-        env.TEST_MBED_OS_ATECC608A_EXAMPLES = 'true'
-    }
-    env.MBED_OS_REPO = 'git@github.com:ARMmbed/mbed-os.git'
-    env.MBED_OS_BRANCH = 'master'
-    env.MBED_OS_TLS_EXAMPLES_REPO = 'git@github.com:ARMmbed/mbed-os-example-tls.git'
-    env.MBED_OS_TLS_EXAMPLES_BRANCH = 'master'
-    env.MBED_OS_CRYPTO_EXAMPLES_REPO = 'git@github.com:ARMmbed/mbed-os-example-mbed-crypto.git'
-    env.MBED_OS_CRYPTO_EXAMPLES_BRANCH = 'master'
-    env.MBED_OS_ATECC608A_EXAMPLES_REPO = 'git@github.com:ARMmbed/mbed-os-example-atecc608a.git'
-    env.MBED_OS_ATECC608A_EXAMPLES_BRANCH = 'master'
-    env.MBED_CRYPTO_BRANCH = env.CHANGE_BRANCH
+    set_common_pr_production_environment()
+    set_crypto_pr_production_environment()
 }
 
 def set_tls_pr_environment() {
     env.JOB_TYPE = 'PR'
     env.TARGET_REPO = 'tls'
     env.REPO_TO_CHECKOUT = 'tls'
-    if (['development', 'development-restricted'].contains(CHANGE_TARGET)) {
+    set_common_pr_production_environment()
+    set_tls_pr_production_environment()
+}
+
+def set_common_pr_production_environment() {
+    env.MBED_OS_REPO = 'git@github.com:ARMmbed/mbed-os.git'
+    env.MBED_OS_BRANCH = 'master'
+    env.MBED_OS_TLS_EXAMPLES_REPO = 'git@github.com:ARMmbed/mbed-os-example-tls.git'
+    env.MBED_OS_TLS_EXAMPLES_BRANCH = 'master'
+    if (['development', 'development-restricted', 'feature-psa'].contains(CHANGE_TARGET)) {
         env.TEST_MBED_OS_AUTHCRYPT_EXAMPLE = 'true'
         env.TEST_MBED_OS_BENCHMARK_EXAMPLE = 'true'
         env.TEST_MBED_OS_HASHING_EXAMPLE = 'true'
         env.TEST_MBED_OS_TLS_CLIENT_EXAMPLE = 'true'
     }
-    env.MBED_OS_REPO = 'git@github.com:ARMmbed/mbed-os.git'
-    env.MBED_OS_BRANCH = 'master'
-    env.MBED_OS_TLS_EXAMPLES_REPO = 'git@github.com:ARMmbed/mbed-os-example-tls.git'
-    env.MBED_OS_TLS_EXAMPLES_BRANCH = 'master'
+}
+
+def set_crypto_pr_production_environment() {
+    env.MBED_CRYPTO_BRANCH = env.CHANGE_BRANCH
+    env.MBED_OS_CRYPTO_EXAMPLES_REPO = 'git@github.com:ARMmbed/mbed-os-example-mbed-crypto.git'
+    env.MBED_OS_CRYPTO_EXAMPLES_BRANCH = 'master'
+    env.MBED_OS_ATECC608A_EXAMPLES_REPO = 'git@github.com:ARMmbed/mbed-os-example-atecc608a.git'
+    env.MBED_OS_ATECC608A_EXAMPLES_BRANCH = 'master'
+    if (['development', 'development-restricted', 'feature-psa'].contains(CHANGE_TARGET)) {
+        env.TEST_MBED_OS_CRYPTO_EXAMPLES = 'true'
+        env.TEST_MBED_OS_ATECC608A_EXAMPLES = 'true'
+    }
+}
+
+def set_tls_pr_production_environment() {
     env.MBED_TLS_BRANCH = env.CHANGE_BRANCH
 }
 
