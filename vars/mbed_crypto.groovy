f184591464d98d3e01465dc417182fabd1ddf46d
Darryl Green <darryl.green@arm.com>
2019-12-13 10:28:52 +0000

    Remove unnecessary node nesting
    Unless we need to run code in a workspace, we don't need to
    allocate a node. Remove the node and deleteDir() calls
    where we aren't running code in workspaces.

--- vars/mbed_crypto.groovy
+++ vars/mbed_crypto.groovy
@@ -1,101 +1,98 @@
 def run_crypto_tests() {
-    node {
-        try {
-            deleteDir()
-            def jobs = [:]
+    try {
+        def jobs = [:]
 
-            /* Linux jobs */
-            if (env.RUN_LINUX_SCRIPTS == "true") {
-                jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
-                    'std-make',
-                    common.linux_platforms,
-                    common.all_compilers,
-                    scripts.std_make_test_sh
-                )
-                jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
-                    'std-make-full-config',
-                    common.linux_platforms,
-                    common.all_compilers,
-                    scripts.std_make_full_config_test_sh
-                )
-                jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
-                    'cmake',
-                    common.linux_platforms,
-                    common.all_compilers,
-                    scripts.cmake_test_sh
-                )
-                jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
-                    'cmake-full',
-                    common.linux_platforms,
-                    common.gcc_compilers,
-                    scripts.cmake_full_test_sh
-                )
-                jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
-                    'cmake-asan',
-                    common.linux_platforms,
-                    common.asan_compilers,
-                    scripts.cmake_asan_test_sh
+        /* Linux jobs */
+        if (env.RUN_LINUX_SCRIPTS == "true") {
+            jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
+                'std-make',
+                common.linux_platforms,
+                common.all_compilers,
+                scripts.std_make_test_sh
+            )
+            jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
+                'std-make-full-config',
+                common.linux_platforms,
+                common.all_compilers,
+                scripts.std_make_full_config_test_sh
+            )
+            jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
+                'cmake',
+                common.linux_platforms,
+                common.all_compilers,
+                scripts.cmake_test_sh
+            )
+            jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
+                'cmake-full',
+                common.linux_platforms,
+                common.gcc_compilers,
+                scripts.cmake_full_test_sh
+            )
+            jobs = jobs + gen_jobs.gen_docker_jobs_foreach(
+                'cmake-asan',
+                common.linux_platforms,
+                common.asan_compilers,
+                scripts.cmake_asan_test_sh
+            )
+        }
+
+        /* BSD jobs */
+        if (env.RUN_FREEBSD == "true") {
+            jobs = jobs + gen_jobs.gen_node_jobs_foreach(
+                'gmake',
+                common.bsd_platforms,
+                common.bsd_compilers,
+                scripts.gmake_test_sh
+            )
+            jobs = jobs + gen_jobs.gen_node_jobs_foreach(
+                'cmake',
+                common.bsd_platforms,
+                common.bsd_compilers,
+                scripts.cmake_test_sh
+            )
+        }
+
+        /* Windows jobs */
+        if (env.RUN_WINDOWS_TEST == "true") {
+            jobs = jobs + gen_jobs.gen_windows_jobs_for_pr()
+        }
+
+        /* All.sh jobs */
+        if (env.RUN_ALL_SH == "true") {
+            for (component in common.all_sh_components['ubuntu-16.04']) {
+                jobs = jobs + gen_jobs.gen_all_sh_jobs(
+                    'ubuntu-16.04', component
                 )
             }
-
-            /* BSD jobs */
-            if (env.RUN_FREEBSD == "true") {
-                jobs = jobs + gen_jobs.gen_node_jobs_foreach(
-                    'gmake',
-                    common.bsd_platforms,
-                    common.bsd_compilers,
-                    scripts.gmake_test_sh
-                )
-                jobs = jobs + gen_jobs.gen_node_jobs_foreach(
-                    'cmake',
-                    common.bsd_platforms,
-                    common.bsd_compilers,
-                    scripts.cmake_test_sh
+            for (component in (common.all_sh_components['ubuntu-18.04'] -
+                               common.all_sh_components['ubuntu-16.04'])) {
+                jobs = jobs + gen_jobs.gen_all_sh_jobs(
+                    'ubuntu-18.04', component
                 )
             }
+        }
 
-            /* Windows jobs */
-            if (env.RUN_WINDOWS_TEST == "true") {
-                jobs = jobs + gen_jobs.gen_windows_jobs_for_pr()
-            }
+        if (env.RUN_ABI_CHECK == "true") {
+            jobs = jobs + gen_jobs.gen_abi_api_checking_job('ubuntu-16.04')
+        }
 
-            /* All.sh jobs */
-            if (env.RUN_ALL_SH == "true") {
-                for (component in common.all_sh_components['ubuntu-16.04']) {
-                    jobs = jobs + gen_jobs.gen_all_sh_jobs(
-                        'ubuntu-16.04', component
-                    )
-                }
-                for (component in (common.all_sh_components['ubuntu-18.04'] -
-                                   common.all_sh_components['ubuntu-16.04'])) {
-                    jobs = jobs + gen_jobs.gen_all_sh_jobs(
-                        'ubuntu-18.04', component
-                    )
-                }
-            }
+        /* Deciding whether to run example jobs is handled within this */
+        jobs = jobs + gen_jobs.gen_all_example_jobs()
 
-            if (env.RUN_ABI_CHECK == "true") {
-                jobs = jobs + gen_jobs.gen_abi_api_checking_job('ubuntu-16.04')
-            }
-
-            /* Deciding whether to run example jobs is handled within this */
-            jobs = jobs + gen_jobs.gen_all_example_jobs()
-
-            jobs.failFast = false
-            parallel jobs
-            if (env.BRANCH_NAME) {
-                githubNotify context: "${env.BRANCH_NAME} Crypto Testing",
-                             description: 'All tests passed',
-                             status: 'SUCCESS'
-            }
-        } catch (err) {
-            echo "Caught: ${err}"
-            currentBuild.result = 'FAILURE'
-            if (env.BRANCH_NAME) {
-                githubNotify context: "${env.BRANCH_NAME} Crypto Testing",
-                             description: 'Test failure',
-                             status: 'FAILURE'
-            }
+        jobs.failFast = false
+        parallel jobs
+        if (env.BRANCH_NAME) {
+            githubNotify context: "${env.BRANCH_NAME} Crypto Testing",
+                         description: 'All tests passed',
+                         status: 'SUCCESS'
+        }
+    } catch (err) {
+        echo "Caught: ${err}"
+        currentBuild.result = 'FAILURE'
+        if (env.BRANCH_NAME) {
+            githubNotify context: "${env.BRANCH_NAME} Crypto Testing",
+                         description: 'Test failure',
+                         status: 'FAILURE'
         }
     }
 }
@@ -115,23 +112,21 @@ def run_pr_job(is_production=true) {
                          status: 'PENDING'
         }
         stage('pre-test-checks') {
-            node {
-                try {
-                    environ.set_crypto_pr_environment(is_production)
-                    common.get_all_sh_components(['ubuntu-16.04', 'ubuntu-18.04'])
-                    if (env.BRANCH_NAME) {
-                        githubNotify context: "${env.BRANCH_NAME} Pre Test Checks",
-                                     description: 'OK',
-                                     status: 'SUCCESS'
-                    }
-                } catch (err) {
-                    if (env.BRANCH_NAME) {
-                        githubNotify context: "${env.BRANCH_NAME} Pre Test Checks",
-                                     description: 'Base branch out of date. Please rebase',
-                                     status: 'FAILURE'
-                    }
-                    throw (err)
+            try {
+                environ.set_crypto_pr_environment(is_production)
+                common.get_all_sh_components(['ubuntu-16.04', 'ubuntu-18.04'])
+                if (env.BRANCH_NAME) {
+                    githubNotify context: "${env.BRANCH_NAME} Pre Test Checks",
+                                 description: 'OK',
+                                 status: 'SUCCESS'
                 }
+            } catch (err) {
+                if (env.BRANCH_NAME) {
+                    githubNotify context: "${env.BRANCH_NAME} Pre Test Checks",
+                                 description: 'Base branch out of date. Please rebase',
+                                 status: 'FAILURE'
+                }
+                throw (err)
             }
         }
 
--- vars/mbed_os_example.groovy
+++ vars/mbed_os_example.groovy
@@ -1,11 +1,8 @@
 def run_pr_job(example) {
     timestamps {
-        node {
-            deleteDir()
-            environ.set_mbed_os_example_pr_environment(example)
-            def jobs = gen_jobs.gen_all_example_jobs()
-            jobs.failFast = false
-            parallel jobs
-        }
+        environ.set_mbed_os_example_pr_environment(example)
+        def jobs = gen_jobs.gen_all_example_jobs()
+        jobs.failFast = false
+        parallel jobs
     }
 }
