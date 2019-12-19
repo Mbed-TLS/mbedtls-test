a563e6b17aa4776cb637660287e6f5309fc53412
Darryl Green <darryl.green@arm.com>
2019-12-19 13:46:34 +0000

    Use docker image when getting components
    The all.sh component check was mistakenly being run on the
    container host, rather than within the container that runs the
    tests. Run the check within the container to get the proper list.

--- vars/gen_jobs.groovy
+++ vars/gen_jobs.groovy
@@ -435,8 +435,8 @@ def gen_release_jobs() {
     }
 
     if (RUN_ALL == "true") {
-        common.get_all_sh_components()
-        for (component in common.all_sh_components) {
+        common.get_all_sh_components(['ubuntu-16.04'])
+        for (component in common.all_sh_components['ubuntu-16.04']) {
             jobs = jobs + gen_all_sh_jobs('ubuntu-16.04', component)
         }
         jobs = jobs + gen_all_sh_jobs('ubuntu-18.04', 'build_mingw')
