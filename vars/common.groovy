e8cf9a6c5925b1a6a3a58c06027e180632060c88
Darryl Green <darryl.green@arm.com>
2020-09-10 11:16:15 +0100

    Switch to use the ECR in IPG-CE-OSS-MbedTLS-prod

--- vars/common.groovy
+++ vars/common.groovy
@@ -16,7 +16,7 @@ import groovy.transform.Field
     'cc' : 'cc'
 ]
 
-@Field docker_repo = '853142832404.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls'
+@Field docker_repo = '666618195821.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls'
 
 @Field one_platform = ["debian-9-x64"]
 @Field linux_platforms = ["debian-9-i386", "debian-9-x64"]
