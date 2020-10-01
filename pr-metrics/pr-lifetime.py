847983a7f2d2185d9f36085184322dcc96ea1c22
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2020-10-01 12:45:13 +0200

    WIP
    Issues:
    - graph readability
    - more importantly, what's the lifetime of a PR that's still open? This is
      already potentially a problem for the median, but even more so for the 75th
    or 90th centiles.

--- pr-metrics/pr-lifetime.py
+++ pr-metrics/pr-lifetime.py
@@ -6,6 +6,7 @@
 from prs import pr_dates, quarter
 
 from collections import defaultdict
+from numpy import percentile
 
 import matplotlib.pyplot as plt
 
@@ -27,35 +28,32 @@ for q in quarters:
     lifetimes_all[q].sort()
     lifetimes_com[q].sort()
 
+c50_all = tuple(percentile(lifetimes_all[q], 50) for q in quarters)
+c50_com = tuple(percentile(lifetimes_com[q], 50) for q in quarters)
 
-def median(sl):
-    """Return the median value of a sorted list of numbers (0 if empty)."""
-    index = (len(sl) - 1) / 2
-    if index < 0:
-        return 0
-    if int(index) == index:
-        return sl[int(index)]
+c75_all = tuple(percentile(lifetimes_all[q], 75) for q in quarters)
+c75_com = tuple(percentile(lifetimes_com[q], 75) for q in quarters)
 
-    i, j = int(index - 0.5), int(index + 0.5)
-    return (sl[i] + sl[j]) / 2
-
-
-med_all = tuple(median(lifetimes_all[q]) for q in quarters)
-med_com = tuple(median(lifetimes_com[q]) for q in quarters)
+c90_all = tuple(percentile(lifetimes_all[q], 90) for q in quarters)
+c90_com = tuple(percentile(lifetimes_com[q], 90) for q in quarters)
 
 fig, ax = plt.subplots()
-ax.plot(quarters, med_all, "b-", label="median overall")
-ax.plot(quarters, med_com, "r-", label="median community")
+ax.plot(quarters, c50_all, "b-", label="median overall")
+ax.plot(quarters, c50_com, "r-", label="median community")
+#ax.plot(quarters, c75_all, "b--", label="3rd quartile overall")
+#ax.plot(quarters, c75_com, "r--", label="3rd quartile community")
+ax.plot(quarters, c90_all, "b:", label="90th centile overall")
+ax.plot(quarters, c90_com, "r:", label="90th centile community")
 ax.legend(loc="upper right")
 ax.grid(True)
 ax.set_xlabel("quarter")
-ax.set_ylabel("median lifetime in days of PRs created that quarter")
+ax.set_ylabel("median/c75/c90 lifetime in days of PRs created that quarter")
 bot, top = ax.set_ylim()
 ax.set_ylim(0, min(365, top))  # we don't care about values over 1 year
-fig.suptitle("Median lifetime of PRs per quarter (less is better)")
+fig.suptitle("Median/c75/c90 lifetime of PRs per quarter (less is better)")
 fig.set_size_inches(12.8, 7.2)  # default 100 dpi -> 720p
 fig.savefig("prs-lifetime.png")
 
 print("Quarter,median overall,median community")
-for q, a, c in zip(quarters, med_all, med_com):
+for q, a, c in zip(quarters, c50_all, c50_com):
     print("{},{},{}".format(q, int(a), int(c)))
