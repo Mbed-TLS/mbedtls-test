e1141532496a79cd36a53197b6e1b98837ceab76
Bence Szépkúti <bence.szepkuti@arm.com>
2021-07-08 17:01:18 +0200

    Simplify dockerfile build scripts

--- vars/dockerfile_builder.groovy
+++ vars/dockerfile_builder.groovy
@@ -23,20 +23,16 @@ def run_job() {
                             if (BUILD_16_04_DOCKERFILE == "true") {
                                 dir('ubuntu-16.04') {
                                     sh """\
-\$(aws ecr get-login) && docker pull 666618195821.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls:ubuntu-16.04
-\$(aws ecr get-login) && docker build -t jenkins-mbedtls:${TAG_FOR_16_04_DOCKERFILE} .
-docker tag jenkins-mbedtls:${TAG_FOR_16_04_DOCKERFILE} 666618195821.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls:${TAG_FOR_16_04_DOCKERFILE}
-\$(aws ecr get-login) && docker push 666618195821.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls:${TAG_FOR_16_04_DOCKERFILE}
+docker build -t $common.docker_repo:${TAG_FOR_16_04_DOCKERFILE} .
+\$(aws ecr get-login) && docker push $common.docker_repo:${TAG_FOR_16_04_DOCKERFILE}
 """
                                 }
                             }
                             if (BUILD_18_04_DOCKERFILE == "true") {
                                 dir('ubuntu-18.04') {
                                     sh """\
-\$(aws ecr get-login) && docker pull 666618195821.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls:ubuntu-18.04
-\$(aws ecr get-login) && docker build -t jenkins-mbedtls:${TAG_FOR_18_04_DOCKERFILE} .
-docker tag jenkins-mbedtls:${TAG_FOR_18_04_DOCKERFILE} 666618195821.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls:${TAG_FOR_18_04_DOCKERFILE}
-\$(aws ecr get-login) && docker push 666618195821.dkr.ecr.eu-west-1.amazonaws.com/jenkins-mbedtls:${TAG_FOR_18_04_DOCKERFILE}
+docker build -t $common.docker_repo:${TAG_FOR_18_04_DOCKERFILE} .
+\$(aws ecr get-login) && docker push $common.docker_repo:${TAG_FOR_18_04_DOCKERFILE}
 """
                                 }
                             }
