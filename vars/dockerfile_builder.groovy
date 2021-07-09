8e3a44d8d1ef711caeb9dfe90e102ae629809e25
Bence Szépkúti <bence.szepkuti@arm.com>
2021-07-10 01:48:47 +0200

    Handle new job parameters

--- vars/dockerfile_builder.groovy
+++ vars/dockerfile_builder.groovy
@@ -1,11 +1,15 @@
 def run_job() {
     timestamps {
         jobs = [:]
-        if (BUILD_16_04_DOCKERFILE == 'true') {
-            jobs += gen_jobs.gen_dockerfile_builder_job(MBED_TLS_TEST_REPO, MBED_TLS_TEST_BRANCH, 'ubuntu-16.04', TAG_FOR_16_04_DOCKERFILE)
+        if (ENABLE_16_04_DOCKERFILE == 'true') {
+            if (ACTION_FOR_16_04_DOCKERFILE == 'build') {
+                jobs += gen_jobs.gen_dockerfile_builder_job(MBED_TLS_TEST_REPO, MBED_TLS_TEST_BRANCH, 'ubuntu-16.04', TAG_FOR_16_04_DOCKERFILE)
+            }
         }
-        if (BUILD_18_04_DOCKERFILE == 'true') {
-            jobs += gen_jobs.gen_dockerfile_builder_job(MBED_TLS_TEST_REPO, MBED_TLS_TEST_BRANCH, 'ubuntu-18.04', TAG_FOR_18_04_DOCKERFILE)
+        if (ENABLE_18_04_DOCKERFILE == 'true') {
+            if (ACTION_FOR_18_04_DOCKERFILE == 'build') {
+                jobs += gen_jobs.gen_dockerfile_builder_job(MBED_TLS_TEST_REPO, MBED_TLS_TEST_BRANCH, 'ubuntu-18.04', TAG_FOR_18_04_DOCKERFILE)
+            }
         }
         jobs.failFast = false
         parallel jobs
