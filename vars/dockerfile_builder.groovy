8654fb82d43489baa5239ac5995f0b6edb4b1d85
Bence Szépkúti <bence.szepkuti@arm.com>
2021-08-03 18:17:17 +0200

    Revert "Add option to delete docker tags"
    This reverts commit 5321fb874f7915086ae91f97c6ec4f4e349b1a24.
    
    We don't have the permissions to delete the images, so revert.

--- vars/dockerfile_builder.groovy
+++ vars/dockerfile_builder.groovy
@@ -2,7 +2,6 @@ def run_job() {
     timestamps {
         stage('dockerfile-builder') {
             jobs = [:]
-            delete_tags = []
             switch (DOCKER_IMAGE_16_04_ACTION) {
                 case 'build':
                     jobs += gen_jobs.gen_dockerfile_builder_job(TEST_REPO, TEST_BRANCH, 'ubuntu-16.04', DOCKER_IMAGE_16_04_TAG)
@@ -10,8 +9,6 @@ def run_job() {
                 case 'publish':
                     jobs += gen_jobs.gen_docker_image_publisher_job('ubuntu-16.04', DOCKER_IMAGE_16_04_TAG)
                     break
-                case 'delete':
-                    delete_tags += [DOCKER_IMAGE_16_04_TAG]
             }
             switch (DOCKER_IMAGE_18_04_ACTION) {
                 case 'build':
@@ -20,10 +17,7 @@ def run_job() {
                 case 'publish':
                     jobs += gen_jobs.gen_docker_image_publisher_job('ubuntu-18.04', DOCKER_IMAGE_18_04_TAG)
                     break
-                case 'delete':
-                    delete_tags += [DOCKER_IMAGE_18_04_TAG]
             }
-            jobs += gen_jobs.gen_docker_batch_delete_tags_job(delete_tags)
             jobs.failFast = false
             parallel jobs
         }
