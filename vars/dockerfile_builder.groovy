323150e80c5c0289d63ad4827d8682f1bf79e1e9
Bence Szépkúti <bence.szepkuti@arm.com>
2021-08-03 18:32:06 +0200

    Extract duplicated code into function

--- vars/dockerfile_builder.groovy
+++ vars/dockerfile_builder.groovy
@@ -1,23 +1,21 @@
+def gen_job_for_action(action, platform, tag) {
+    jobs = [:]
+    switch (action) {
+        case 'build':
+            jobs += gen_jobs.gen_dockerfile_builder_job(TEST_REPO, TEST_BRANCH, platform, tag)
+            break
+        case 'publish':
+            jobs += gen_jobs.gen_docker_image_publisher_job(platform, tag)
+            break
+    }
+    return jobs
+}
+
 def run_job() {
     timestamps {
         stage('dockerfile-builder') {
-            jobs = [:]
-            switch (DOCKER_IMAGE_16_04_ACTION) {
-                case 'build':
-                    jobs += gen_jobs.gen_dockerfile_builder_job(TEST_REPO, TEST_BRANCH, 'ubuntu-16.04', DOCKER_IMAGE_16_04_TAG)
-                    break
-                case 'publish':
-                    jobs += gen_jobs.gen_docker_image_publisher_job('ubuntu-16.04', DOCKER_IMAGE_16_04_TAG)
-                    break
-            }
-            switch (DOCKER_IMAGE_18_04_ACTION) {
-                case 'build':
-                    jobs += gen_jobs.gen_dockerfile_builder_job(TEST_REPO, TEST_BRANCH, 'ubuntu-18.04', DOCKER_IMAGE_18_04_TAG)
-                    break
-                case 'publish':
-                    jobs += gen_jobs.gen_docker_image_publisher_job('ubuntu-18.04', DOCKER_IMAGE_18_04_TAG)
-                    break
-            }
+            jobs  = gen_job_for_action(DOCKER_IMAGE_16_04_ACTION, 'ubuntu-16.04', DOCKER_IMAGE_16_04_TAG)
+            jobs += gen_job_for_action(DOCKER_IMAGE_18_04_ACTION, 'ubuntu-18.04', DOCKER_IMAGE_18_04_TAG)
             jobs.failFast = false
             parallel jobs
         }
