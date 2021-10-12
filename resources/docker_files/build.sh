bab0bf627ed846239fa0e187a766b612e2097393
Gilles Peskine <Gilles.Peskine@arm.com>
2021-10-12 12:47:08 +0200

    Script to build one or more docker image locally

---
+++ resources/docker_files/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Build the specified Dockerfile(s).
+# Follow the image naming convention used on Jenkins.
+
+set -e
+
+if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
+    cat <<EOF
+Usage: $0 DIR/Dockerfile[...]
+Build the specified Docker images.
+EOF
+    exit
+fi
+
+list_sh="$(dirname -- "$0")/list-docker-image-tags.sh"
+
+build () {
+    if [ -d "$1" ]; then
+        set -- "$1/Dockerfile"
+    fi
+    tag="$("$list_sh" "$1")"
+    sudo sudo docker build --network=host -t "$tag" -f "$1" "${1%/*}"
+}
+
+for d in "$@"; do
+    build "$d"
+done
