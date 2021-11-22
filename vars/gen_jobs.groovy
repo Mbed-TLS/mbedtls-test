23140f5362f8f18a413c529c93dff0808ae7149d
Darryl Green <darryl.green@arm.com>
2021-11-22 14:46:33 +0000

    Use FreeBSD test node

--- vars/gen_jobs.groovy
+++ vars/gen_jobs.groovy
@@ -133,7 +133,7 @@ export PYTHON=/usr/local/bin/python2.7
 def node_label_for_platform(platform) {
     switch (platform) {
     case ~/^(debian|ubuntu)(-.*)?/: return 'container-host';
-    case ~/^freebsd(-.*)?/: return 'freebsd';
+    case ~/^freebsd(-.*)?/: return 'freebsd-test';
     case ~/^windows(-.*)?/: return 'windows';
     default: return platform;
     }
