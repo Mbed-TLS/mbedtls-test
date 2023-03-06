d93b2555703099a73aa7f1989193074b1e56a929
Yanray Wang <yanray.wang@arm.com>
2023-03-06 15:19:02 +0800

    Add support of ubuntu-22.04 image in groovy script
    Signed-off-by: Yanray Wang <yanray.wang@arm.com>

--- vars/dockerfile_builder.groovy
+++ vars/dockerfile_builder.groovy
@@ -15,6 +15,7 @@ def run_job() {
             def jobs = gen_job_for_action(DOCKER_IMAGE_16_04_ACTION, 'ubuntu-16.04')
             jobs += gen_job_for_action(DOCKER_IMAGE_18_04_ACTION, 'ubuntu-18.04')
             jobs += gen_job_for_action(DOCKER_IMAGE_20_04_ACTION, 'ubuntu-20.04')
+            jobs += gen_job_for_action(DOCKER_IMAGE_22_04_ACTION, 'ubuntu-22.04')
             jobs.failFast = false
             parallel jobs
         }
