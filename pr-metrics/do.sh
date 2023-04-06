c86237c304a410cf17ef032b29bb24511b0f67e0
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2023-04-06 11:12:22 +0200

    New script pr-backlog.py
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- pr-metrics/do.sh
+++ pr-metrics/do.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-for topic in created closed pending lifetime; do
+for topic in created closed pending lifetime backlog; do
     echo "PRs $topic..."
     rm -f prs-${topic}.png prs-${topic}.csv
     ./pr-${topic}.py > prs-${topic}.csv
