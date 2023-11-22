99d33e96105443398c32f810b40afc57104d64d7
Bence Szépkúti <bence.szepkuti@arm.com>
2023-11-22 11:27:38 +0100

    Increase Windows testing timeout
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- vars/common.groovy
+++ vars/common.groovy
@@ -51,7 +51,7 @@ import org.mbed.tls.jenkins.BranchInfo
 @Field perJobTimeout = [
         time: 120,
         raasOffset: 17,
-        windowsTestingOffset: 0,
+        windowsTestingOffset: 60,
         unit: 'MINUTES'
 ]
 
