76867d7440a9e6c7df297085c34b1327b0608af9
Bence Szépkúti <bence.szepkuti@arm.com>
2023-12-12 14:13:20 +0100

    Build and run tests on AArch64 Docker images
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- vars/gen_jobs.groovy
+++ vars/gen_jobs.groovy
@@ -67,28 +67,20 @@ Map<String, Callable<Void>> gen_simple_windows_jobs(BranchInfo info, String labe
 
 def node_label_for_platform(platform) {
     switch (platform) {
-    case ~/^(debian|ubuntu)(-.*)?/: return 'container-host';
-    case 'arm-compilers': return 'container-host';
+    case ~/^(debian|ubuntu|arm-compilers).*-amd64/: return 'container-host';
+    case ~/^(debian|ubuntu|arm-compilers).*-arm64/: return 'container-host-arm64';
     case ~/^freebsd(-.*)?/: return 'freebsd';
     case ~/^windows(-.*)?/: return 'windows';
     default: return platform;
     }
 }
 
-def platform_has_docker(platform) {
-    if (platform == 'arm-compilers') {
-        return true
-    }
-    def os = platform.replaceFirst(/-.*/, "")
-    return ['debian', 'ubuntu'].contains(os)
+boolean platform_has_docker(String platform) {
+    return platform.startsWithAny('debian', 'ubuntu', 'arm-compilers')
 }
 
 def platform_lacks_tls_tools(platform) {
-    if (platform == 'arm-compilers') {
-        return true
-    }
-    def os = platform.replaceFirst(/-.*/, "")
-    return ['freebsd'].contains(os)
+    return platform.startsWithAny('freebsd', 'arm-compilers')
 }
 
 // gen_docker_job(info, job_name, platform, script_in_docker, ...)
@@ -678,9 +670,10 @@ def gen_release_jobs(BranchInfo info, String label_prefix='', boolean run_exampl
 }
 
 def gen_dockerfile_builder_job(String platform, boolean overwrite=false) {
-    def dockerfile = libraryResource "docker_files/$platform/Dockerfile"
-
-    def tag = "$platform-${common.git_hash_object(dockerfile)}"
+    def (image, arch) = platform.split(/-(?=[^-]*$)/)
+    def dockerfile = libraryResource "docker_files/$image/Dockerfile"
+    def tag = "$image-${common.git_hash_object(dockerfile)}-$arch"
+    def cache = "$image-cache-$arch"
     def check_docker_image
     if (common.is_open_ci_env) {
         check_docker_image = "docker manifest inspect $common.docker_repo:$tag > /dev/null 2>&1"
@@ -693,7 +686,8 @@ def gen_dockerfile_builder_job(String platform, boolean overwrite=false) {
     return job(platform) {
         /* Take the lock on the master node, so we don't tie up an executor while waiting */
         lock(tag) {
-            analysis.node_record_timestamps('dockerfile-builder', platform) {
+            def node_label = arch == 'amd64' ? 'dockerfile-builder' : "container-host-$arch"
+            analysis.node_record_timestamps(node_label, platform) {
                 def image_exists = false
                 if (!overwrite) {
                     image_exists = sh(script: check_docker_image, returnStatus: true) == 0
@@ -728,7 +722,7 @@ aws ecr get-login-password | docker login --username AWS --password-stdin $commo
 """
                         }
 
-                        analysis.record_inner_timestamps('dockerfile-builder', platform) {
+                        analysis.record_inner_timestamps(node_label, platform) {
                             sh """\
 # Use BuildKit and a remote build cache to pull only the reuseable layers
 # from the last successful build for this platform
@@ -736,13 +730,13 @@ DOCKER_BUILDKIT=1 docker build \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
     --build-arg DOCKER_REPO=$common.docker_repo \
     $extra_build_args \
-    --cache-from $common.docker_repo:$platform-cache \
+    --cache-from $common.docker_repo:$cache \
     -t $common.docker_repo:$tag \
-    -t $common.docker_repo:$platform-cache .
+    -t $common.docker_repo:$cache .
 
 # Push the image with its unique tag, as well as the build cache tag
 docker push $common.docker_repo:$tag
-docker push $common.docker_repo:$platform-cache
+docker push $common.docker_repo:$cache
 """
                         }
                     }
