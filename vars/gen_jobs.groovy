8f41d52cf60a9996341fd8e1ecbf76402891cb1b
Bence Szépkúti <bence.szepkuti@arm.com>
2024-02-02 19:54:57 +0100

    Upgrade to Arm Compiler 5.06u7 and 6.21
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- vars/gen_jobs.groovy
+++ vars/gen_jobs.groovy
@@ -691,12 +691,26 @@ aws ecr get-login-password | docker login --username AWS --password-stdin $commo
                             sh 'docker buildx create --name dockerfile-builder --use'
                         }
 
+                        // Generate URL for armclang
+                        if (platform == 'arm-compilers') {
+                            withCredentials(common.is_open_ci_env ? [] : [aws(credentialsId: 'armclang-readonly-keys')]) {
+                                sh '''
+set -eux
+aws s3 presign --expires-in 300 \
+    s3://trustedfirmware-private/armclang/ARMCompiler_506_Linux_x86_b960.tar.gz >armc5_url
+aws s3 presign --expires-in 300 \
+    s3://trustedfirmware-private/armclang/ARMCompiler6.21_standalone_linux-x86_64.tar.gz >armc6_url
+'''
+                                extra_build_args +=
+                                    ' --secret id=armc5_url,src=./armc5_url --secret id=armc6_url,src=./armc6_url'
+                            }
+                        }
+
                         analysis.record_inner_timestamps('dockerfile-builder', platform) {
                             sh """\
 # Use BuildKit and a remote build cache to pull only the reuseable layers
 # from the last successful build for this platform
 docker buildx build \
-    --build-arg DOCKER_REPO=$common.docker_repo \
     $extra_build_args \
     --cache-from $common.docker_repo:$platform-cache \
     --cache-to type=registry,mode=max,image-manifest=true,oci-mediatypes=true,ref=$common.docker_repo:$platform-cache \
