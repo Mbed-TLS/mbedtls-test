4f2fa31cd7088758b3d2eadc2f3e1985ffed3e37
Bence Szépkúti <bence.szepkuti@arm.com>
2024-10-24 13:05:38 +0200

    Fix platform name
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- vars/analysis.groovy
+++ vars/analysis.groovy
@@ -281,7 +281,7 @@ tests/scripts/analyze_outcomes.py '$outcomes_csv'
         return gen_jobs.gen_docker_job(info,
                                        job_name,
                                        'helper-container-host',
-                                       'ubuntu-22.04',
+                                       'ubuntu-22.04-amd64',
                                        script_in_docker,
                                        post_checkout: post_checkout,
                                        post_execution: post_execution)
