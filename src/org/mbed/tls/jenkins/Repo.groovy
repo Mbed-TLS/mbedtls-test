7542abc27f26a8c189dd76432adb6b81216a9037
Bence Szépkúti <bence.szepkuti@arm.com>
2025-02-21 19:25:14 +0100

    Fix CPS missing method
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- src/org/mbed/tls/jenkins/Repo.groovy
+++ src/org/mbed/tls/jenkins/Repo.groovy
@@ -1,8 +1,17 @@
 package org.mbed.tls.jenkins
 
+import com.cloudbees.groovy.cps.NonCPS
+
 enum Repo {
     TLS,
     TF_PSA_CRYPTO,
     FRAMEWORK,
-    EXAMPLE
+    EXAMPLE,
+
+    // The Groovy CPS code generator fails to declare this implicit method,
+    // which causes runtime exceptions.
+    @NonCPS
+    static <T extends Enum<T>> T valueOf(Class<T> clazz, String name) {
+        return Enum.valueOf(clazz, name)
+    }
 }
