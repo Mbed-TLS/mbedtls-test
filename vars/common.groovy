d3c54b91337fa4fb316917e65a5817bd1a511025
Darryl Green <darryl.green@arm.com>
2025-05-06 10:13:22 +0100

    Explicitly set region

--- vars/common.groovy
+++ vars/common.groovy
@@ -196,7 +196,7 @@ docker pull $docker_repo:$docker_image
 """
             else
                 sh """\
-aws ecr get-login-password | docker login --username AWS --password-stdin $docker_ecr
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $docker_ecr
 docker pull $docker_repo:$docker_image
 """
             break
