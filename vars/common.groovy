9434619e827f258167d66bff9c184e2a9397ef8c
Gilles Peskine <Gilles.Peskine@arm.com>
2026-07-30 16:15:37 +0200

    Remove dead code for retired CI instances
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- vars/common.groovy
+++ vars/common.groovy
@@ -44,7 +44,7 @@ import org.mbed.tls.jenkins.BranchInfo
 @Field final boolean is_legacy_open_ci_env = false
 @Field final boolean is_openci_env = true
 
-@Field final String ci_name = is_legacy_open_ci_env ? 'TF OpenCI (legacy)' : is_openci_env ? 'TF OpenCI' : 'Internal CI'
+@Field final String ci_name = 'TF OpenCI'
 
 /*
  * This controls the timeout each job has. It does not count the time spent in
@@ -67,9 +67,9 @@ import org.mbed.tls.jenkins.BranchInfo
     'cc' : 'cc'
 ]
 
-@Field final String docker_repo_name = (is_legacy_open_ci_env || is_openci_env) ? 'docker.io/trustedfirmware/ci-amd64-mbed-tls-ubuntu' : 'jenkins-mbedtls'
-@Field final String docker_ecr = is_openci_env ? env.PRIVATE_CONTAINER_REGISTRY : '666618195821.dkr.ecr.eu-west-1.amazonaws.com'
-@Field final String docker_repo = is_legacy_open_ci_env ? docker_repo_name : "$docker_ecr/$docker_repo_name"
+@Field final String docker_repo_name = 'docker.io/trustedfirmware/ci-amd64-mbed-tls-ubuntu'
+@Field final String docker_ecr = env.PRIVATE_CONTAINER_REGISTRY
+@Field final String docker_repo = "$docker_ecr/$docker_repo_name"
 
 /* List of Linux platforms. When a job can run on multiple Linux platforms,
  * it runs on the first element of the list that supports this job. */
@@ -105,7 +105,7 @@ import org.mbed.tls.jenkins.BranchInfo
  * @return The return value of the closure
  */
 def <T> T mbedtls_node(String label, Closure<T> body) {
-    return node(is_openci_env ? "mbedtls-$label" : label, body)
+    return node("mbedtls-$label", body)
 }
 
 /* Compute the git object ID of the Dockerfile.
@@ -207,12 +207,7 @@ def get_docker_image(platform) {
     def docker_image = get_docker_tag(platform)
     for (int attempt = 1; attempt <= 3; attempt++) {
         try {
-            if (is_legacy_open_ci_env)
-                sh """\
-docker pull $docker_repo:$docker_image
-"""
-            else
-                sh """\
+            sh """\
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $docker_ecr
 docker pull $docker_repo:$docker_image
 """
@@ -401,8 +396,7 @@ void check_every_all_sh_component_will_be_run(Collection<BranchInfo> infos) {
  * context (optional): a short string identifying which part of the job this is
  *                     a status for. GitHub only shows the latest state and
  *                     description for a given context. If it is omitted, this
- *                     method determines the correct context from is_open_ci_env
- *                     and BRANCH_NAME.
+ *                     method determines the correct context from BRANCH_NAME.
  */
 void maybe_notify_github(String state, String description, String context=null) {
     if (!env.BRANCH_NAME) {
@@ -501,7 +495,7 @@ $emailbody
 
 @NonCPS
 boolean pr_author_has_write_access(String repo_name, int pr) {
-    String credentials = (is_legacy_open_ci_env || is_openci_env) ? 'mbedtls-github-token' : 'd015f9b1-4800-4a81-86b3-9dbadc18ee00'
+    String credentials = 'mbedtls-github-token'
     def github = Connector.connect(null, Connector.lookupScanCredentials(currentBuild.rawBuild.parent, null, credentials))
     def repo = github.getRepository(repo_name)
     return repo.getPermission(repo.getPullRequest(pr).user) in [GHPermissionType.ADMIN, GHPermissionType.WRITE]
