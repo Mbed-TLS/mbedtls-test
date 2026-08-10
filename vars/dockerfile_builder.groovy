1fd38553592aad331d89bbd9858a04be2cd4a562
Gilles Peskine <Gilles.Peskine@arm.com>
2026-08-10 11:38:22 +0200

    Fix the mbedtls-dockerfile-builder job to match the job config
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- vars/dockerfile_builder.groovy
+++ vars/dockerfile_builder.groovy
@@ -1,20 +1,25 @@
-def gen_job_for_action(action, platform) {
-    switch (action) {
-        case 'build':
-            return gen_jobs.gen_dockerfile_builder_job(platform, true)
-        case 'skip':
-            return [:]
-        default:
-            throw new IllegalArgumentException(action)
-    }
-}
-
 def run_job() {
     timestamps {
         stage('dockerfile-builder') {
-            def jobs = gen_job_for_action(DOCKER_IMAGE_16_04_ACTION, 'ubuntu-16.04-amd64')
-            jobs += gen_job_for_action(DOCKER_IMAGE_18_04_ACTION, 'ubuntu-18.04-amd64')
-            jobs += gen_job_for_action(DOCKER_IMAGE_24_04_ACTION, 'ubuntu-24.04-amd64')
+            def jobs = [:]
+            if (params.BUILD_UBUNTU_16_04_AMD64_DOCKER_IMAGE) {
+                jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-16.04-amd64', true)
+            }
+            if (params.BUILD_UBUNTU_18_04_AMD64_DOCKER_IMAGE) {
+                jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-18.04-amd64', true)
+            }
+            if (params.BUILD_UBUNTU_18_04_ARM64_DOCKER_IMAGE) {
+                jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-18.04-arm64', true)
+            }
+            if (params.BUILD_UBUNTU_24_04_AMD64_DOCKER_IMAGE) {
+                jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-24.04-amd64', true)
+            }
+            if (params.BUILD_UBUNTU_24_04_ARM64_DOCKER_IMAGE) {
+                jobs += gen_jobs.gen_dockerfile_builder_job('ubuntu-24.04-arm64', true)
+            }
+            if (params.BUILD_ARM_COMPILERS_AMD64_DOCKER_IMAGE) {
+                jobs += gen_jobs.gen_dockerfile_builder_job('arm-compilers-amd64', true)
+            }
             jobs.failFast = false
             parallel jobs
         }
